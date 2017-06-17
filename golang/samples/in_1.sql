9c62af21057bdf2d356869b0df162808e5b177a8
julian nymark <julianymark@gmail.com>
2017-06-17 23:21:42 +0200

    :poop:

--- golang/samples/in_1.sql
+++ golang/samples/in_1.sql
@@ -1,15 +1,1 @@
-SAVEPOINT test;
-    DO $dollar$
-    BEGIN
-        BEGIN
-            PERFORM meme_factory._generate_big_meme('bad-luck-brian', 1000, 1000);
-        EXCEPTION
-        WHEN raise_exception THEN
-            IF SQLERRM IS DISTINCT FROM 'unlucky' THEN
-                RAISE EXCEPTION 'Expected exception "unlucky", got: % %', SQLSTATE, SQLERRM;
-            END IF;
-            RETURN;
-        END;
-        RAISE EXCEPTION 'Expect exception "unlucky" for generation of meme "bad-luck-brian".';
-    END; $dollar$;
-ROLLBACK TO SAVEPOINT test;
+SAVEPOINT test; DO $dollar$ BEGIN BEGIN PERFORM meme_factory._generate_big_meme('bad-luck-brian', 1000, 1000); EXCEPTION WHEN raise_exception THEN IF SQLERRM IS DISTINCT FROM 'unlucky' THEN RAISE EXCEPTION 'Expected exception "unlucky", got: % %', SQLSTATE, SQLERRM; END IF; RETURN; END; RAISE EXCEPTION 'Expect exception "unlucky" for generation of meme "bad-luck-brian".'; END; $dollar$; ROLLBACK TO SAVEPOINT test;
